03fced9454e39123b2cc4cea5109b2b8dc3a610b
effilin <effienummer@gmail.com>
2024-05-24 11:38:05 -0400

    starting features

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # calculatorApp
 this is a calculator app
+requirements:
+1. get data( 2 numbers)
+2. choose the operator
+3. get the result
+4. reset the screen
+
+design:
+https://www.figma.com/design/CwGbv6wS65HvVB4Viqmh4B/Untitled?node-id=0%3A1&t=MjJUxXNX7COGYRfV-1
---
+++ index.js
@@ -0,0 +1,9 @@
+const addition=(a, b) => ((a*10) +(b* 10))/10;
+const subtract=(a, b) => a-b;
+const multiply=(a, b) => a*b;
+const divide=(a, b) => a/b
+
+console.log(addition(.1, .2));
+console.log(subtract(.2, .1));
+console.log(multiply(2, 1));
+console.log(divide(3, 0));

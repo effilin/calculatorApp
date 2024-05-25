5a9e5314940ec6038f147a1cf850515f0f69328c
effilin <effienummer@gmail.com>
2024-05-25 15:51:30 -0400

    calapp

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
+
 const addition=(a, b) => ((a*10) +(b* 10))/10;
 const subtract=(a, b) => a-b;
 const multiply=(a, b) => a*b;
 const divide=(a, b) => a/b
 
-console.log(addition(.1, .2));
-console.log(subtract(.2, .1));
-console.log(multiply(2, 1));
-console.log(divide(3, 0));
+
+const numbers1 = [];
+
+function value(a) {
+    numbers1.push(a);
+    const inputData = numbers1.toString();
+    document.getElementById('results').innerText=inputData;
+}
